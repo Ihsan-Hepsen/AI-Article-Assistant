d4a199688b6bda58b8340169c8b729d3f16afbc0
ihsan-hepsen <ihsanhepsen@gmail.com>
2024-01-16 22:11:59 -0600

    small changes

--- content.js
+++ content.js
@@ -19,7 +19,6 @@
   }
 
   function createFloatingButton(selectedText) {
-    // Create a button element
     button = document.createElement('button')
     button.innerText = 'Summarize'
     button.style.position = 'fixed'
@@ -27,28 +26,22 @@
     button.style.right = '10px'
     button.style.zIndex = '9999'
 
-    // Attach click event to the button
     button.addEventListener('click', () => {
       chrome.runtime.sendMessage({ action: 'summarizeText', text: selectedText })
       removeButton()
     })
 
-    // Append the button to the document body
     document.body.appendChild(button)
-
-    // Add a click event listener to the document to remove the button if clicked outside
     document.addEventListener('click', handleDocumentClick)
   }
 
   function handleDocumentClick(event) {
-    // Check if the click target is not the button
     if (button && !button.contains(event.target)) {
       removeButton()
     }
   }
 
   function removeButton() {
-    // Remove the button and event listener
     if (button) {
       button.remove()
       button = null
